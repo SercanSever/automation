009565d72f3c16395db225629928ec712710f8f2
Sercan Sever <sercan.sever16@gmail.com>
2021-03-04 23:26:48 +0300

    04.03.21

--- AutomationUI/Controllers/InvoiceDetailsController.cs
+++ AutomationUI/Controllers/InvoiceDetailsController.cs
@@ -1,4 +1,5 @@
 ﻿using Automation.Business.Abstract;
+using Automation.Entities.Concrete;
 using AutomationUI.Models;
 using System;
 using System.Collections.Generic;
@@ -25,6 +26,36 @@ namespace AutomationUI.Controllers
                 InvoiceDetails = _invoiceDetailManager.GetAll()
             };
             return View(model);
+        }
+        [HttpGet]
+        public ActionResult AddInvoiceDetail()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddInvoiceDetail(InvoiceDetail invoiceDetail)
+        {
+            _invoiceDetailManager.Add(invoiceDetail);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult DeleteInvoice(int id)
+        {
+            var invoiceDetail = _invoiceDetailManager.GetById(id);
+            _invoiceDetailManager.Update(invoiceDetail);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateInvoiceDetail(int id)
+        {
+            var invoiceDetail = _invoiceDetailManager.GetById(id);
+            return View("UpdateInvoiceDetail", invoiceDetail);
+        }
+        [HttpPost]
+        public ActionResult UpdateInvoiceDetail(InvoiceDetail invoiceDetail)
+        {
+            _invoiceDetailManager.Update(invoiceDetail);
+            return RedirectToAction("Index");
         }
     }
 }
