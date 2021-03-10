0cd04244adc238221446f26cfd94d9ea30d868df
Sercan Sever <sercan.sever16@gmail.com>
2021-03-10 21:24:38 +0300

    10.03.21

--- Automation.DataAccess/Context/AutomationContext.cs
+++ Automation.DataAccess/Context/AutomationContext.cs
@@ -1,25 +1,10 @@
 ﻿using Automation.Entities.Concrete;
-using System;
-using System.Collections.Generic;
 using System.Data.Entity;
-using System.Text;
 
 namespace Automation.DataAccess.Context
 {
     public class AutomationContext : DbContext
     {
-        //private DbSet<Product> products;
-
-        //public DbSet<Product> GetProducts()
-        //{
-        //    return products;
-        //}
-
-        //public void SetProducts(DbSet<Product> value)
-        //{
-        //    products = value;
-        //}
-
         public DbSet<Customer> Customers { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
@@ -30,6 +15,5 @@ namespace Automation.DataAccess.Context
         public DbSet<InvoiceDetail> InvoiceDetails { get; set; }
         public DbSet<SalesDetail> SalesDetails { get; set; }
         public DbSet<Admin> Admins { get; set; }
-
     }
 }
--- Automation.Entities/Concrete/Product.cs
+++ Automation.Entities/Concrete/Product.cs
@@ -1,4 +1,4 @@
-﻿using Automation.Core.Entities;
+﻿    using Automation.Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
